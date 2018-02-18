2999c21be14594757d2994c2631620c8b0d260cf
Sachin <sachin.tibrewal@innoraft.com>
2018-02-18 23:08:36 +0530

    using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,29 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var cssMin = require('gulp-css');
 var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
 gulp.task('css',function(){
 
   gulp.src([
-    'stylesheets/combined_css.css',
-    'stylesheets/app.css'
+    './stylesheets/combined_css.css',
+    './stylesheets/app.css'
   ])
     .pipe(concat('app.css'))
     .pipe(cssMin())
-    .pipe(gulp.dest('stylesheets'))
+    .pipe(gulp.dest('./stylesheets'))
 
 });
 
-gulp.task('default', ['css']);
+
+gulp.task("htmlmin", function(){
+  gulp.src("./index.html")
+  .pipe(htmlmin({collapseWhitespace:true}))
+  .pipe(gulp.dest("minified"));
+
+
+
+});
+gulp.task('default', [
+  'css',
+  'htmlmin'
+]);
